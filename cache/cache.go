d353ea4ea06de894babd064cfb5a08ef91104466
Bhargav4037 <113230592+Bhargav4037@users.noreply.github.com>
2024-06-24 06:34:42 +0000

    Add files via upload

---
+++ cache/cache.go
@@ -0,0 +1,21 @@
+package cache
+
+import (
+	"fmt"
+
+	"github.com/gomodule/redigo/redis"
+)
+
+func Chack() {
+	chg()
+}
+
+func chg() {
+	ghj()
+}
+
+func ghj() {
+	var tyg redis.Conn
+	fmt.Println(tyg)
+	//maindb.Check()
+}
